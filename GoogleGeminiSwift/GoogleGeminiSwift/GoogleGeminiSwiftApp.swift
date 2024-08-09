19345d7803b42eb8aea4903dfabf7395313068eb
Sunil Gurung <Gurungboi@users.noreply.github.com>
2024-08-09 14:11:54 +1000

    Add files via upload

---
+++ GoogleGeminiSwift/GoogleGeminiSwift/GoogleGeminiSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleGeminiSwiftApp.swift
+//  GoogleGeminiSwift
+//
+//  Created by Sunil Gurung on 9/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct GoogleGeminiSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
